fa2d982814f04f03f8db18eef15d45418d03b8c0
Oscar Caballero <ocaballeror@gmail.com>
2018-05-26 13:20:45 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+quit(){
+	echo "$1" >&2
+	[ $# -ge 2 ] && exit "$2"
+	exit 1
+}
+
+cwd=$PWD
+
+cd $(dirname $0)
+[ -f .apksigner ] || quit "Missing file: $PWD/.apksigner"
+
+skipgit=false
+while [ $# -gt 0 ]; do
+	if [ "$1" = "--skip-git" ]; then
+		skipgit=true
+		shift
+	fi
+done
+
+if ! $skipgit; then
+	git fetch --all
+	git co master || quit 'Git pull failed'
+	git pull || quit 'Git pull failed'
+	output="$(git rebase upstream/master)"
+	[ "$?" = 0 ] || { git rebase --abort; quit 'Git rebase failed'; }
+	[ "$output" == "Current branch master is up to date." ] && quit "$output" 0
+fi
+
+[ "$JAVA_HOME" ] || export JAVA_HOME='/usr/lib/jvm/java-8-openjdk-amd64'
+[ "$ANDROID_HOME" ] || export ANDROID_HOME='/usr/lib/android-sdk'
+
+./gradlew assembleRelease || quit 'Compilation failure'
+cp .apksigner app/build/outputs/apk/release/
+cd app/build/outputs/apk/release/
+if [ ! -f app-release-unsigned.apk ]; then
+	ls -lh
+	quit 'Apk file was not created'
+fi
+
+[ -f app-release-aligned.apk ] && rm app-release-aligned.apk
+[ -f phonograph.apk ] && rm phonograph.apk
+
+
+zipalign -v -p 4 app-release-unsigned.apk app-release-aligned.apk || quit 'Zipalign failed'
+apksigner sign --ks /home/oscar/.keystore.jks --out phonograph.apk app-release-aligned.apk <.apksigner || quit 'Apksigner failed'
+mv phonograph.apk /srv/sftp/phonograph-$(date '+%d%m%y').apk
+
+cd $cwd
